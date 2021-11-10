920776acfda6bbc387a2f2fbc99b4e4c3a5ffa5e
Asain Satanov <satanov.asain@mail.ru>
2021-11-10 20:10:44 +0500

    Соз-е МенюКарт из Б/Д. fetch API & JSON used

--- js/main.js
+++ js/main.js
@@ -141,7 +141,7 @@ window.addEventListener('DOMContentLoaded', function () {
             this.descr = descr;
             this.price = price;
             this.parent = document.querySelector(parentSelector);
-            this.course = 12;
+            this.course = 200;
             this.changeToTenge();
         }
 
@@ -164,27 +164,40 @@ window.addEventListener('DOMContentLoaded', function () {
             this.parent.append(cardElement);
         }
     }
+     async function getMenuCard(url){
+        const res = await fetch(url);
+        if (!res.ok){
+            throw new Error(`Данные с ${url} не приняты. Статус - ${res.status}`);
+        }
+        return await res.json();
+     }
 
-    new MenuCard("img/tabs/vegy.jpg",
-     "vegy",
-     '"Фитнес"',
-     'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-     229,
-     '.menu__field div'
-     ).renderMenu();
-    new MenuCard("img/tabs/elite.jpg",
-    "elite",
-    '“Премиум”',
-    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',550,
-    '.menu__field div'
-    ).renderMenu();
-    new MenuCard("img/tabs/post.jpg",
-    "post",
-    '"Постное"',
-    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-    430,
-    '.menu__field div'
-    ).renderMenu();
+     getMenuCard('http://localhost:3000/menu')
+     .then(dataSeries=>{
+         dataSeries.forEach(({img,altimg,title,descr,price})=>{
+            new MenuCard(img,altimg,title,descr,price,'.menu__field div').renderMenu();
+         });
+     });
+    // new MenuCard("img/tabs/vegy.jpg",
+    //  "vegy",
+    //  '"Фитнес"',
+    //  'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    //  229,
+    //  '.menu__field div'
+    //  ).renderMenu();
+    // new MenuCard("img/tabs/elite.jpg",
+    // "elite",
+    // '“Премиум”',
+    // 'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',550,
+    // '.menu__field div'
+    // ).renderMenu();
+    // new MenuCard("img/tabs/post.jpg",
+    // "post",
+    // '"Постное"',
+    // 'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+    // 430,
+    // '.menu__field div'
+    // ).renderMenu();
 
     /* //This КОНТЕКСТ ВЫЗОВА - ПРИМЕР СЦЕПКИ ОБЪЕКТА К ФУНКЦИИ(ПЕРЕДАЧА ОБЪЕКТА КАК АРГУМЕНТА В ФУНКЦИЮ)
     function greeting(eventParty){
